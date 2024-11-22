0eb4df3c8c4e15a4675058d6f812649b1b301bcf
rahulbats <rahulbats@gmail.com>
2024-11-21 22:57:39 -0600

    Update CustomDataCollator.py

--- CustomDataCollator.py
+++ CustomDataCollator.py
@@ -3,7 +3,7 @@ from transformers import DataCollatorWithPadding, DefaultDataCollator
 class DataCollatorWithIds(DefaultDataCollator):
     def __call__(self, features):
         # Extract `example_id` from features
-        example_ids = [feature.pop("example_ids", None) for feature in features]
+        example_ids = [feature.pop("example_id", None) for feature in features]
 
         # Call the parent collator for padding
         batch = super().__call__(features)
